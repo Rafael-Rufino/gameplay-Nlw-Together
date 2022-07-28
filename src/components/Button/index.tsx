80b2b425234837949ee8e80109949f53c8cbec1e
RafaelRufino <rafaelrufino.52@gmail.com>
2022-07-28 16:54:28 -0300

    feat: compoenet de button

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { RectButtonProps } from "react-native-gesture-handler";
+
+import { Text, ButtonContainer, IconWrapper, ImageIcon } from "./styles";
+import DiscordImg from "../../assets/discord.png";
+
+type Props = RectButtonProps & {
+  title: string;
+};
+
+export function Button({ title, ...rest }: Props) {
+  return (
+    <ButtonContainer {...rest}>
+      <IconWrapper>
+        <ImageIcon source={DiscordImg} />
+      </IconWrapper>
+      <Text>{title}</Text>
+    </ButtonContainer>
+  );
+}
