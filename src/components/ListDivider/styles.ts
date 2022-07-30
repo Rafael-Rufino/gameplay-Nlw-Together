352fa4898445781c42331faa7835822eb77a28ed
RafaelRufino <rafaelrufino.52@gmail.com>
2022-07-30 00:47:17 -0300

    feat: criando component de divider

---
+++ src/components/ListDivider/styles.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components/native";
+import theme from "../../global/styles/theme";
+
+export const Container = styled.View`
+  width: 80%;
+  height: 2px;
+  background-color: ${theme.colors.secondary40};
+  margin-vertical: 21px;
+  align-self: flex-end;
+`;
