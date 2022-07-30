76a4c8a9c16e9cf9de5d55f43d40bdc21adc378f
RafaelRufino <rafaelrufino.52@gmail.com>
2022-07-30 00:44:50 -0300

    feat: criando tipagem deo navigation

---
+++ src/@types/navigation.d.ts
@@ -0,0 +1,9 @@
+export declare global {
+  namespace ReactNavigation {
+    interface RootParamList {
+      Home: undefined;
+      SignIn: undefined;
+      details: { orderId: string };
+    }
+  }
+}
