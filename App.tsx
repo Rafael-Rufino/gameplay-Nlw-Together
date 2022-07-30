2dbab820aa321ac78de21bd06dd9bfbcd0a9e58a
RafaelRufino <rafaelrufino.52@gmail.com>
2022-07-30 00:49:49 -0300

    refactor: definindo componente principal

--- App.tsx
+++ App.tsx
@@ -1,20 +1,40 @@
 import React from "react";
 import { ThemeProvider } from "styled-components";
 import { StatusBar } from "react-native";
-import { SignIn } from "./src/screens/SignIn";
 import theme from "./src/global/styles/theme";
+import AppLoading from "expo-app-loading";
+
+import {
+  useFonts,
+  Inter_400Regular,
+  Inter_500Medium,
+} from "@expo-google-fonts/inter";
+
+import {
+  Rajdhani_500Medium,
+  Rajdhani_700Bold,
+} from "@expo-google-fonts/rajdhani";
+import { Routes } from "./src/routes";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({
+    Inter_400Regular,
+    Inter_500Medium,
+    Rajdhani_500Medium,
+    Rajdhani_700Bold,
+  });
+
+  if (!fontsLoaded) {
+    return <AppLoading />;
+  }
   return (
-    <>
-      <ThemeProvider theme={theme}>
-        <StatusBar
-          barStyle="light-content"
-          backgroundColor="transparent"
-          translucent
-        />
-        <SignIn />
-      </ThemeProvider>
-    </>
+    <ThemeProvider theme={theme}>
+      <StatusBar
+        barStyle="light-content"
+        backgroundColor="transparent"
+        translucent
+      />
+      <Routes />
+    </ThemeProvider>
   );
 }
