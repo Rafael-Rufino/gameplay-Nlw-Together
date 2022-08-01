4e8e9acd23d1204f6ae10f1a2be3077aac9b2d28
RafaelRufino <rafaelrufino.52@gmail.com>
2022-08-01 19:33:40 -0300

    feat: criando component de categorias

--- src/components/Category/index.tsx
+++ src/components/Category/index.tsx
@@ -1,6 +1,6 @@
 import React from "react";
-import { LinearGradient } from "expo-linear-gradient";
-import { RectButtonProps } from "react-native-gesture-handler";
+
+import { RectButton, RectButtonProps } from "react-native-gesture-handler";
 import { SvgProps } from "react-native-svg";
 
 import theme from "../../global/styles/theme";
@@ -8,27 +8,34 @@ import { View } from "react-native";
 type Props = RectButtonProps & {
   title: string;
   icon: React.FC<SvgProps>;
+  hasCheckBox?: boolean;
   checked?: boolean;
 };
 
-import { ButtonCategory, Content, Title, styles } from "./styles";
+import { LinearGradientContainer, Content, Title, styles } from "./styles";
 
 export function Category({
   title,
   icon: Icon,
   checked = false,
+  hasCheckBox = false,
   ...rest
 }: Props) {
-  const { secondary50, secondary70 } = theme.colors;
+  const { secondary40, secondary50, secondary70, secondary85 } = theme.colors;
   return (
-    <ButtonCategory {...rest}>
-      <LinearGradient colors={[secondary50, secondary70]}>
-        <Content style={{ opacity: checked ? 1 : 0.4 }}>
-          <View style={checked ? styles.checked : styles.check} />
+    <RectButton {...rest}>
+      <LinearGradientContainer colors={[secondary50, secondary70]}>
+        <Content
+          style={{ opacity: checked ? 1 : 0.5 }}
+          colors={[checked ? secondary85 : secondary50, secondary40]}
+        >
+          {hasCheckBox && (
+            <View style={checked ? styles.checked : styles.check} />
+          )}
           <Icon width={40} height={48} />
           <Title>{title}</Title>
         </Content>
-      </LinearGradient>
-    </ButtonCategory>
+      </LinearGradientContainer>
+    </RectButton>
   );
 }
--- src/components/Category/styles.ts
+++ src/components/Category/styles.ts
@@ -1,9 +1,9 @@
-import { RectButton } from "react-native-gesture-handler";
+import { LinearGradient } from "expo-linear-gradient";
 import { StyleSheet } from "react-native";
 import styled from "styled-components/native";
 import theme from "../../global/styles/theme";
 
-export const ButtonCategory = styled(RectButton)`
+export const LinearGradientContainer = styled(LinearGradient)`
   width: 104px;
   height: 120px;
   justify-content: center;
@@ -11,37 +11,39 @@ export const ButtonCategory = styled(RectButton)`
   border-radius: 8px;
   margin-right: 8px;
 `;
-export const Content = styled.View`
-  width: 100;
-  height: 116;
-  backgroundcolor: ${theme.colors.secondary40};
+export const Content = styled(LinearGradient)`
+  width: 100px;
+  height: 116px;
   border-radius: 8px;
   justify-content: space-between;
   align-items: center;
-  padding-vertical: 7px;
+  padding-vertical: 20px;
 `;
 export const Title = styled.Text`
-  font-family: ${theme.fonts.title500};
+  font-family: ${theme.fonts.title700};
   color: ${theme.colors.heading};
   font-size: 16px;
+  margin-top: 16px;
 `;
 export const styles = StyleSheet.create({
   check: {
+    position: "absolute",
+    top: 7,
+    right: 7,
     width: 12,
     height: 12,
     backgroundColor: theme.colors.secondary100,
-    alignSelf: "flex-end",
-    marginRight: 7,
     borderColor: theme.colors.secondary50,
     borderWidth: 2,
     borderRadius: 2,
   },
   checked: {
+    position: "absolute",
+    top: 7,
+    right: 7,
     width: 10,
     height: 10,
     backgroundColor: theme.colors.primary,
-    alignSelf: "flex-end",
-    marginRight: 7,
     borderRadius: 2,
   },
 });
