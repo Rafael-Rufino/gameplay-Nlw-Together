421d711dafe6b4d1c1d5efe42c0b4e2b7aefd105
RafaelRufino <rafaelrufino.52@gmail.com>
2022-08-01 19:32:44 -0300

    feat: criando component de listdivider

--- src/components/ListDivider/styles.ts
+++ src/components/ListDivider/styles.ts
@@ -2,9 +2,10 @@ import styled from "styled-components/native";
 import theme from "../../global/styles/theme";
 
 export const Container = styled.View`
-  width: 80%;
+  width: 75%;
   height: 2px;
   background-color: ${theme.colors.secondary40};
-  margin-vertical: 21px;
+  margin-vertical: 32px;
+  margin-top: 2px;
   align-self: flex-end;
 `;
