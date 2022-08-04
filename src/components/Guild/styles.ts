ee917b8dd826ae3e160a47324e1d1e3fad8fda4d
RafaelRufino <rafaelrufino.52@gmail.com>
2022-08-04 00:55:38 -0300

    style: ajuste no estilo

--- src/components/Guild/styles.ts
+++ src/components/Guild/styles.ts
@@ -6,26 +6,26 @@ export const TouchableOpacityContainer = styled(TouchableOpacity)`
   width: 100%;
   flex-direction: row;
   align-items: center;
-  padding: 24px;
+  padding-horizontal: 24px;
 `;
 
 export const Content = styled.View`
   flex: 1;
   justify-content: center;
+  margin-left: 20px;
 `;
 
 export const Title = styled.Text`
   font-family: ${theme.fonts.title700};
   color: ${theme.colors.heading};
   font-size: 18px;
-  margin-bottom: 11px;
+  margin-bottom: 4px;
 `;
 
 export const Type = styled.Text`
   font-family: ${theme.fonts.text400};
   color: ${theme.colors.highlight};
   font-size: 13px;
-  margin-bottom: 24px;
 `;
 
 export const Header = styled.View``;
