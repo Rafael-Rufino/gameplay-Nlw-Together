38bf6d665f4052b0b57ca430e702a5e39d09ab86
RafaelRufino <rafaelrufino.52@gmail.com>
2022-08-04 00:56:36 -0300

    refactor: ajuste no component de divider

--- src/components/ListDivider/index.tsx
+++ src/components/ListDivider/index.tsx
@@ -2,6 +2,16 @@ import React from "react";
 
 import { Container } from "./styles";
 
-export function ListDivider() {
-  return <Container />;
+type Props = {
+  isCentered?: boolean;
+};
+
+export function ListDivider({ isCentered }: Props) {
+  return (
+    <Container
+      style={
+        isCentered ? { marginVertical: 12 } : { marginTop: 2, marginBottom: 31 }
+      }
+    />
+  );
 }
--- src/components/ListDivider/styles.ts
+++ src/components/ListDivider/styles.ts
@@ -2,10 +2,8 @@ import styled from "styled-components/native";
 import theme from "../../global/styles/theme";
 
 export const Container = styled.View`
-  width: 75%;
+  width: 78%;
   height: 2px;
   background-color: ${theme.colors.secondary40};
-  margin-vertical: 32px;
-  margin-top: 2px;
   align-self: flex-end;
 `;
