f7ba5ccf6d1190eeaefa976be00acbde006b5140
RafaelRufino <rafaelrufino.52@gmail.com>
2022-08-05 03:23:52 -0300

    refactor: ajuste nas rodas

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 import { NavigationContainer } from "@react-navigation/native";
 
-import { AuthRoutes } from "./auth.routes";
+import { AppRoutes } from "./app.routes";
 
 import { useAuth } from "../hooks/auth";
 
@@ -13,7 +13,7 @@ export function Routes() {
 
   return (
     <NavigationContainer>
-      {user.id ? <AuthRoutes /> : <SignIn />}
+      {user ? <AppRoutes /> : <SignIn />}
     </NavigationContainer>
   );
 }
