41da6ab0ed59d19a9d62a0c00e2f161900b1d0e6
RafaelRufino <rafaelrufino.52@gmail.com>
2022-08-05 01:47:42 -0300

    refactor: configurando rotas privas e publica

--- src/routes/auth.routes.tsx
+++ src/routes/auth.routes.tsx
@@ -4,7 +4,7 @@ import theme from "../global/styles/theme";
 
 import { createStackNavigator } from "@react-navigation/stack";
 import { Home } from "../screens/Home";
-import { SignIn } from "../screens/SignIn";
+
 import { AppointmentDetails } from "../screens/AppointmentDetails";
 import { AppointmentCreate } from "../screens/AppointmentCreate";
 import { Guilds } from "../screens/Guilds";
@@ -19,7 +19,6 @@ export function AuthRoutes() {
         cardStyle: { backgroundColor: theme.colors.secondary100 },
       }}
     >
-      <Screen name="SignIn" component={SignIn} />
       <Screen name="Home" component={Home} />
       <Screen name="AppointmentDetails" component={AppointmentDetails} />
       <Screen name="AppointmentCreate" component={AppointmentCreate} />
