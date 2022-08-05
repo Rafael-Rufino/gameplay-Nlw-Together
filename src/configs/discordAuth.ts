7e3d76eae4596dbf2beaf33bb6aff14b6f1ff7c8
RafaelRufino <rafaelrufino.52@gmail.com>
2022-08-05 01:46:33 -0300

    feat: criando variaveis referente api

---
+++ src/configs/discordAuth.ts
@@ -0,0 +1,11 @@
+const REDIRECT_URI = "https%3A%2F%2Fauth.expo.io%2Fgameplay";
+const SCOPE = "identify%20email%20connections%20guilds";
+const RESPONSE_TYPE = "token";
+const CLIENT_ID = "858595294093770762";
+const CDN_IMAGE = "https://cdn.discordapp.com";
+
+export { REDIRECT_URI, SCOPE, RESPONSE_TYPE, CLIENT_ID, CDN_IMAGE };
+
+// https://auth.expo.io/@anonymous/gameplay-6e3806ea-6741-40a9-93d3-8e76b47a80a7
+
+//https%3A%2F%2Fauth.expo.io%2Fgameplay
