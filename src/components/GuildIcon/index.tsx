8e5bf0cd8914be6a98baf6d3ae769bc865048b2d
RafaelRufino <rafaelrufino.52@gmail.com>
2022-08-05 23:50:51 -0300

    refactor: ajuste de guildicon

--- src/components/GuildIcon/index.tsx
+++ src/components/GuildIcon/index.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { View } from "react-native";
 
 import { Image } from "./styles";
+import DiscordSvg from "../../assets/discord.svg";
 
-export function GuildIcon() {
-  const uri =
-    "https://img.ibxk.com.br/2020/10/30/30110741964176.jpg?w=352&h=208&mode=crop&scale=both";
-  return <Image source={{ uri }} resizeMode="cover" />;
+import { Container } from "./styles";
+
+const { CDN_IMAGE } = process.env;
+type Props = {
+  guildId: string;
+  iconId: string | null;
+};
+
+export function GuildIcon({ guildId, iconId }: Props) {
+  const uri = `${CDN_IMAGE}/icons/${guildId}/${iconId}.png`;
+
+  return (
+    <Container>
+      {iconId ? (
+        <Image source={{ uri }} resizeMode="cover" />
+      ) : (
+        <DiscordSvg width={40} height={40} />
+      )}
+    </Container>
+  );
 }
